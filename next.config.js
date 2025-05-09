38d49165257c370d207f77d1302379c66fbf1bb8
postmanEamon <eamon.cullen@postman.com>
2025-05-09 13:42:47 +0200

    Vercel ignore lint warns

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// ...existing code or create new file...
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
