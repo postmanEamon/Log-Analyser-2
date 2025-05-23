d844c39fbc67d9d9d4c5de6d3f41481236b0094b
postmanEamon <eamon.cullen@postman.com>
2025-05-23 11:41:37 +0200

    Updated tab name and favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import Head from "next/head";
 import LogAnalyzer from '@/components/LogAnalyzer';
 
 export default function Home() {
   return (
-    <main className="min-h-screen p-4">
-      <LogAnalyzer />
-    </main>
+    <>
+      <Head>
+        <title>Log Analyzer</title>
+      </Head>
+      <main className="min-h-screen p-4">
+        <LogAnalyzer />
+      </main>
+    </>
   );
 }
